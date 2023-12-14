477ba998c095d2e2cdc5e82c5958295f2544e16f
Showole Oladayo <show4ril@gmail.com>
2023-12-14 04:18:09 +0100

    changes

--- app/Http/Middleware/NotInstallCheckMiddleware.php
+++ app/Http/Middleware/NotInstallCheckMiddleware.php
@@ -11,15 +11,15 @@ class NotInstallCheckMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        try {
-            DB::connection()->getPdo();
-        } catch (\Exception $e) {
-            return response()->view('install.index');
-        }
+        // try {
+        //     DB::connection()->getPdo();
+        // } catch (\Exception $e) {
+        //     return response()->view('install.index');
+        // }
 
-        if (Schema::hasTable('settings') && Schema::hasTable('users') && isInstalled()) {
-            return redirect('/');
-        }
+        // if (Schema::hasTable('settings') && Schema::hasTable('users') && isInstalled()) {
+        //     return redirect('/');
+        // }
         return $next($request);
     }
 }
